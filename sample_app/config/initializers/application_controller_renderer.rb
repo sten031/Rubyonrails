2643afd3590c326e6d68769d4296e7b2c38adfd7
田中慎吾 <tanakashingo@mbp.local>
2020-02-02 11:08:30 +0900

    [Finish] 3-9

---
+++ sample_app/config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '93c9e9d7dc9de368f8cb16039fd0484f888d09c5c27046768e116e802f431439ade76ea64a5d77c51376d6fe179f6bc2b178a9c85f50b1f2c1b2709c8ba26ffc'
