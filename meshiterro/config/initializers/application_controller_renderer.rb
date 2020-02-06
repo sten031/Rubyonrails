f35d839c299b1c15dd887df4a785c9dfaa284ce6
田中慎吾 <tanakashingo@mbp.local>
2020-02-06 14:11:57 +0900

    [Finish] 5-3

--- meshiterro/config/initializers/application_controller_renderer.rb
+++ meshiterro/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'ae2c753b0a59f13f5c150e5f9a85cfe69f2240eab225583f2c348398d2133659e0257a22d428546e332b8215fb10260fcbb6f29168d4a6bfabbe9cbbf90ec373'
