8ad952c3ae72c6a3e0b43a75a19f03d888e468df
田中慎吾 <tanakashingo@mbp.local>
2020-02-06 19:36:21 +0900

    [Finish] 5-4

--- meshiterro/db/migrate/20200205084258_create_post_images.rb
+++ meshiterro/db/migrate/20200205084258_create_post_images.rb
@@ -2,7 +2,7 @@ class CreatePostImages < ActiveRecord::Migration[5.2]
   def change
     create_table :post_images do |t|
       t.text :shop_name
-      t.string :image_id
+      t.text :image_id
       t.text :caption
       t.integer :user_id
 
--- meshiterro/db/schema.rb
+++ meshiterro/db/schema.rb
@@ -10,11 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_05_084258) do
+ActiveRecord::Schema.define(version: 2020_02_06_072704) do
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "post_image_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "post_comments", force: :cascade do |t|
+    t.text "comment"
+    t.integer "user_id"
+    t.integer "post_image_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "post_images", force: :cascade do |t|
     t.text "shop_name"
-    t.string "image_id"
+    t.text "image_id"
     t.text "caption"
     t.integer "user_id"
     t.datetime "created_at", null: false
